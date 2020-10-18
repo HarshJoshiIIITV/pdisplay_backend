955532b87cb2618d4d7f0958287db118067eae57
Harsh joshi <43510098+HarshJoshiIIITV@users.noreply.github.com>
2020-10-19 00:03:17 +0530

    Create pdisplay_database.sql

---
+++ pdisplay_database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE posts (
+	id serial PRIMARY KEY,
+  title_db text NOT NULL,
+	description_db text NOT NULL,
+	image_db text NO NULL 
+);
+
+
+            TABLE STRUCTURE
+--------------------------------------------------            
+id | title_db  | description_db | image_db
+--------------------------------------------------
+1  | demo1     | abcdef         | https://abcd.jpg    
+2  | demo2     | abcdef         | https://abcd.jpg
+3  | demo3     | abcdef         | https://abcd.jpg
